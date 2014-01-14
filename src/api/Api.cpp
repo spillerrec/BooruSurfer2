d946532257b0351977408e306f0a5b3e1b7f6f94
spillerrec <spillerrec@hotmail.com>
2014-01-14 20:38:01 +0100

    ADD: automatically switch between HTTP and HTTPS ADD: support automatic redirection

--- src/api/Api.cpp
+++ src/api/Api.cpp
@@ -31,36 +31,78 @@
 
 using namespace Poco::Net;
 using namespace Poco;
+using namespace std;
 
-std::string Api::get_from_url( std::string url, std::vector<std::pair<std::string,std::string> > headers ) const{
+static string istream2string( const istream& stream ){
+	return string(static_cast<stringstream const&>(stringstream() << stream.rdbuf()).str());
+}
+
+string http_get( URI uri, HTTPRequest& req, HTTPResponse& res ){
+	cout << "http_get()\n";
+	HTTPClientSession session(uri.getHost(), uri.getPort());
+	session.sendRequest(req);
+	return istream2string( session.receiveResponse(res) );
+}
+
+string https_get( URI uri, HTTPRequest& req, HTTPResponse& res ){
+	cout << "https_get()\n";
+	const auto context = new Context( Context::CLIENT_USE, "", Context::VERIFY_NONE );
+	HTTPSClientSession session(uri.getHost(), uri.getPort(), context);
+	session.sendRequest(req);
+	return istream2string( session.receiveResponse(res) );
+}
+
+string Api::get_from_url( string url, vector<pair<string,string> > headers ) const{
+	if( url.empty() ){
+		cout << "ERROR: no url\n";
+		return "";
+	}
+	
 	try{
-		std::cout << "Url: " << url << "\n";
-		// prepare session
-		Poco::URI uri(url.c_str());
-		//TODO: understand and improve
-		//const Context::Ptr context = new Context(Context::CLIENT_USE, "", "", "", Context::VERIFY_NONE, 9, false, "ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH");
-		//HTTPSClientSession session(uri.getHost(), uri.getPort(), context);
-		HTTPClientSession session(uri.getHost(), uri.getPort());
-
-		// prepare path
-		std::string path(uri.getPathAndQuery());
-		if (path.empty()) path = "/";
-
-		// send request
-		HTTPRequest req(HTTPRequest::HTTP_GET, path, HTTPMessage::HTTP_1_1);
+		URI uri( url );
+		cout << "Url: " << url << " - " << uri.getPathAndQuery() << "\n";
+		
+		HTTPRequest req(HTTPRequest::HTTP_GET, uri.getPathAndQuery(), HTTPMessage::HTTP_1_1);
+		HTTPResponse res;
+		
+		//Add headers
 		for( auto h : headers )
 			req.add( h.first, h.second );
-		session.sendRequest(req);
-
-		// get response
-		HTTPResponse res;
-		//cout << res.getStatus() << " " << res.getReason() << endl;
-
-		// print response
-		std::istream &is = session.receiveResponse(res);
-		return std::string(static_cast<std::stringstream const&>(std::stringstream() << is.rdbuf()).str());
+		req.add( "User-Agent", "Opera/9.80 (Windows NT 6.2; Win64; x64) Presto/2.12.388 Version/12.15" );
+		//TODO: do something with the user-agent
+		
+		//Get response
+		string str = ( url.find( "https://" ) == 0 )
+			?	https_get( uri, req, res )
+			:	http_get( uri, req, res )
+			;
+		
+		switch( res.getStatus() ){
+			case HTTPResponse::HTTP_OK:
+				return str;
+			
+			case HTTPResponse::HTTP_MOVED_PERMANENTLY:
+			case HTTPResponse::HTTP_FOUND:
+			case HTTPResponse::HTTP_SEE_OTHER:
+				if( res.has( "Location" ) ){
+					cout << "Doing redirect, from: " << url << "\n";
+					cout << "                  to: " << res.get( "Location" ) << "\n";
+					return get_from_url( res.get( "Location" ) );
+				}
+				else{
+					cout << "Could not find location to redirect to\n";
+					return "";
+				}
+			
+			default:
+				cout << "Returned with status code: " << res.getStatus() << "\n";
+				cout << str;
+				return "";
+		}
 	}
 	catch( Exception &ex ){
+		cout << "Some exception happened during url request\n";
+		cout << ex.displayText() << "\n";
 		return "";
 	}
 }
